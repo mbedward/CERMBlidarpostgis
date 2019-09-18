4dd6936716fd3010064273a47c26c568820c237b
Michael Bedward <michael.bedward@gmail.com>
2019-09-18 18:20:42 +1000

    remove obsolete code from .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  assign("._cermblidar_settings", list(), pos = 1)
 }
 
 .onUnload <- function(libpath) {
-  if (exists(._cermblidar_settings, where = 1)) rm(._cermblidar_settings, pos = 1)
 }
