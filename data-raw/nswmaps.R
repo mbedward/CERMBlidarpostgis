7738598a83e34de0b7824436ea194112cb905443
Michael Bedward <michael.bedward@gmail.com>
2020-02-20 14:55:13 +1100

    set EPSG code for nswmap100k

--- data-raw/nswmaps.R
+++ data-raw/nswmaps.R
@@ -5,4 +5,7 @@ nswmaps100k <- sf::st_read("data-raw/nswmaps_shapefile/100k_250k.shp")
 nswmaps100k <- nswmaps100k[, c("mapnumber", "maptitle", "geometry")]
 colnames(nswmaps100k)[2] <- "mapname"
 
+# CRS is defined but we need to set the integer EPSG code
+sf::st_crs(nswmaps100k) <- 4326
+
 usethis::use_data(nswmaps100k, overwrite = TRUE)
